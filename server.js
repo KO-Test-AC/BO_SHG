b5ea9e9902ba8c3879001dec1614cb4af74d05e5
KO-Test-AC <omwakak58@gmail.com>
2025-01-26 12:37:46 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 3000;
+
+// Create a MySQL connection using mysql2
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'KENYAkelvin@28*',
+  database: 'bo_database'
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL database!');
+});
+
+// API endpoint to fetch data from MySQL
+app.get('/data', (req, res) => {
+  db.query('SELECT * FROM your_table_name', (err, result) => {
+    if (err) throw err;
+    res.json(result);  // Return the result as JSON
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
